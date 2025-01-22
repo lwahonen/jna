c6c29697a80fc37a2a4e2e709bb5bc75063c7523
Lauri Wiljami Ahonen <lauri.ahonen@diktamen.com>
2025-01-22 12:59:20 +0200

    Remove stale build files

--- 32bit.bat
+++
@@ -1,7 +0,0 @@
-set path=c:\cygwin\bin;%path%
-z:
-
-cd \diktamen\jna
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"  x86
-set JAVA_HOME=C:\Program Files (x86)\Java\jdk1.8.0_181
-ant native -DUSE_MSVC=true
--- compile.bat
+++
@@ -1,4 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat"  x86
-set path=%path%;C:\cygwin64\bin
-cd \asemat\jna
-ant -DUSE_MSVC=true
